8a3ff9f01210f9e9511dd584cad81d949d6a154c
Anndony Quemag <anndony.kommit@gmail.com>
2021-06-24 19:10:15 -0500

    files sections 7, 8, 9, 10 corresponding to Adding a Second Model, Refactoring, Deleting Comments, Security

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
-    validates :title, presence: true #title value must contain at least one non-whitespace character.
-    validates :body, presence: true, length: { minimum: 8 } #body must also be present a value. Also, declare that the bodyvalue must be at least 10 characters long.
-end
+    include Visible
+  
+    has_many :comments, dependent: :destroy
+  
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+  end
+  
