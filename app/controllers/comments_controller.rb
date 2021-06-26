ce2fd0f6af3809cae358c612bf5c8ed54c1059ed
Anndony Quemag <anndony.kommit@gmail.com>
2021-06-25 19:46:22 -0500

    commit about task four corresponding Coding practices

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,29 @@
 class CommentsController < ApplicationController
-
   #http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
-    def create
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.create(comment_params)
-        redirect_to article_path(@article)
-    end
+  def create
+    @article = Article.find(params[:article_id])
 
-    def destroy
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.find(params[:id])
-      @comment.destroy
-      redirect_to article_path(@article)
-    end
+    @comment = @article.comments.create(comment_params)
 
-    private
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+
+    @comment = @article.comments.find(params[:id])
+
+    @comment.destroy
     
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
-    end
+    redirect_to article_path(@article)
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
 
 
 end
